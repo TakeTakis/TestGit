38b700413a4e7937989e6e760149116ad13aeaf7
Take-Takis <axel.perez.gomez@gmail.com>
2022-01-31 22:30:30 -0600

    The first commint in my personal account

---
+++ src/onlinechallenge/g2/ClassPolinomio.java
@@ -0,0 +1,23 @@
+
+package onlinechallenge.g2;
+
+
+public class ClassPolinomio {
+    private int parteNumeral;
+    private int parteAlgebraica;
+     
+
+    public ClassPolinomio(int parteNumeral, int parteAlgebraica) {
+        this.parteNumeral = parteNumeral;
+        this.parteAlgebraica = parteAlgebraica;
+    }
+
+    public int getParteNumeral() {
+        return parteNumeral;
+    }
+
+    public int getParteAlgebraica() {
+        return parteAlgebraica;
+    }
+
+}
